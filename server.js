6eb038310ca38c2297adb317c1eff0ee16ed6ecf
eckzen <eckzen@yahoo.com>
2020-07-21 11:48:31 +0800

    working submit button

--- server.js
+++ server.js
@@ -3,20 +3,28 @@ require("./models/db");
 const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 
 const employeeController = require("./controllers/employeeController");
 
 var app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 app.set("views", path.join(__dirname, "/views/"));
 app.engine(
-  "nhs",
+  "hbs",
   exphbs({
     extname: "hbs",
-    defaultLyout: "mainLayout",
+    defaultLayout: "mainLayout",
     layoutsDir: __dirname + "/views/layouts/",
   })
 );
+
 app.set("view engine", "hbs");
 
 app.listen(3000, () => {
